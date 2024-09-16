f3a29ceeae000b0088a9956da5bea23854e0984c
avinashmyerolkar <avinashmyerolkar@gmail.com>
2024-09-16 08:16:36 +0000

    add second experiment

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -6,7 +6,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/mast
 
 df = df.iloc[:, 3:]  # remove all text containing columns
 
-df = df[df['Length of Membership'] > 3]  # Length of Membership column filter criteria greater than 3
-
+df = df[df['Length of Membership'] > 1]  # Length of Membership column filter criteria for exp2 is greater than 1
+df.drop(columns=['Avg. Session Length'], inplace=True) # drop col for exp 2 
 df.to_csv(os.path.join('data','customer.csv'))
 
